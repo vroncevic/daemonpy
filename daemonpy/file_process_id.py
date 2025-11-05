c54b3d82549781ecaa9c37b9ba4b448c746a7f0d
Vladimir Roncevic <elektron.ronca@gmail.com>
2025-11-05 05:15:26 +0100

    [daemonpy] Updated code coverage, version, docs

--- daemonpy/file_process_id.py
+++ daemonpy/file_process_id.py
@@ -4,7 +4,7 @@
 Module
     file_process_id.py
 Copyright
-    Copyright (C) 2020 - 2025 Vladimir Roncevic <elektron.ronca@gmail.com>
+    Copyright (C) 2020 - 2026 Vladimir Roncevic <elektron.ronca@gmail.com>
     daemonpy is free software: you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by the
     Free Software Foundation, either version 3 of the License, or
@@ -32,10 +32,10 @@ except ImportError as ats_error_message:  # pragma: no cover
     sys.exit(f'\n{__file__}\n{ats_error_message}\n')  # pragma: no cover
 
 __author__: str = 'Vladimir Roncevic'
-__copyright__: str = '(C) 2025, https://vroncevic.github.io/daemonpy'
+__copyright__: str = '(C) 2026, https://vroncevic.github.io/daemonpy'
 __credits__: List[str] = ['Vladimir Roncevic', 'Python Software Foundation']
 __license__: str = 'https://github.com/vroncevic/daemonpy/blob/dev/LICENSE'
-__version__: str = '2.0.6'
+__version__: str = '2.0.7'
 __maintainer__: str = 'Vladimir Roncevic'
 __email__: str = 'elektron.ronca@gmail.com'
 __status__: str = 'Updated'
